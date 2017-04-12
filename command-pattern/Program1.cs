22cad846061d97771eab51ae2fcb34e9f80fc7ac
marcus <ms2asdk@gmail.com>
2017-04-12 12:36:51 -0700

    add base clases

---
+++ command-pattern/Program1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CommandDemo
+{
+    public class Program1
+    {
+        public static void Run(string[] args)
+        {
+            if (args.Length != 0) return;
+            PrintUsage();
+            return;
+        }
+
+        private static void PrintUsage()
+        {
+
+        }
+    }
+}
---
+++ command-pattern/Program2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CommandDemo
+{
+    public class Program2
+    {
+        public static void Run(string[] args)
+        {
+
+        }
+    }
+}
