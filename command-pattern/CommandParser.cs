767c11d2db4f2ba05f4c7d34cd53a4c8d3e6b4a3
marcus <ms2asdk@gmail.com>
2017-04-13 03:52:17 -0700

    add command parser

---
+++ command-pattern/CommandParser.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CommandDemo.Commands;
+
+namespace CommandDemo
+{
+    public class CommandParser
+    {
+        private readonly IEnumerable<ICommandFactory> _availableCommands;
+
+        public CommandParser(IEnumerable<ICommandFactory> availableCommands)
+        {
+            this._availableCommands = availableCommands;
+        }
+
+        internal ICommand ParseCommand(string[] args)
+        {
+            var requestedCommandName = args[0];
+
+            var command = FindRequestedCommand(requestedCommandName);
+            if (null == command)
+                return new NotFoundCommand {Name = requestedCommandName};
+
+            return command.MakeCommand(args);
+        }
+
+        ICommandFactory FindRequestedCommand(string commandName)
+        {
+            return _availableCommands
+                .FirstOrDefault(cmd => cmd.CommandName == commandName);
+        }
+    }
+}
