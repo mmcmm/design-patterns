3904c0cd390a587e58a4b375130d105b07bf3be3
marcus <ms2asdk@gmail.com>
2017-04-13 03:26:29 -0700

    add command factory interface

---
+++ command-pattern/ICommandFactory.cs
@@ -0,0 +1,10 @@
+﻿namespace CommandDemo
+{
+    public interface ICommandFactory
+    {
+        string CommandName { get; }
+        string Description { get; }
+
+        ICommand MakeCommand(string[] arguments);
+    }
+}
