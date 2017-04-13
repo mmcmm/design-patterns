bbfd2f912099aa16b2d37d9e0cda70b5340b03c9
marcus <ms2asdk@gmail.com>
2017-04-13 03:29:58 -0700

    add not found command class

---
+++ command-pattern/NotFoundCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CommandDemo
+{
+    public class NotFoundCommand : ICommand
+    {
+        public string Name { get; set; }
+
+        public void Execute()
+        {
+            Console.WriteLine("Couldn't find command: " + Name);
+        }
+    }
+}
