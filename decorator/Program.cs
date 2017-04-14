3a9dc900bbecb22c23d24c27f76c80a2bae289a9
marcus <ms2asdk@gmail.com>
2017-04-14 02:37:14 -0700

    decorator example

--- decorator/Program.cs
+++ decorator/Program.cs
@@ -7,7 +7,15 @@ namespace DecoratorDemo
     {
         public static void Main(string[] args)
         {
+            Pizza largePizza = new LargePizza();
+            largePizza = new Cheese(largePizza);
+            largePizza = new Ham(largePizza);
+            largePizza = new Peppers(largePizza);
 
+            Console.WriteLine(largePizza.GetDescription());
+            Console.WriteLine("{0:C2}", largePizza.CalculateCost());
+
+            Console.ReadKey();
         }
     }
 }
