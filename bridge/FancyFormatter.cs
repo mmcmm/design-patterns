5b9240931f0529f0b1e5d8d1bfb3257c92782880
marcus <ms2asdk@gmail.com>
2017-04-15 08:57:30 -0700

    fancy formatter

---
+++ bridge/FancyFormatter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bridge
+{
+    public class FancyFormatter : IFormatter
+    {
+        public string Format(string key, string value)
+        {
+            return $"-= {key} ----- =- {value}";
+        }
+    }
+}
