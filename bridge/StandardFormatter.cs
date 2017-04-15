18376282e580274dcfc8f77674649085e5f70398
marcus <ms2asdk@gmail.com>
2017-04-15 08:56:45 -0700

    standard formatter

---
+++ bridge/StandardFormatter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bridge
+{
+    internal class StandardFormatter : IFormatter
+    {
+        public string Format(string key, string value)
+        {
+            return $"{key}: {value}";
+        }
+    }
+}
