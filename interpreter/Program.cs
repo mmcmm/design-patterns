61773834d5547ab5b62cd8be7bd5d5c60d039c6c
marcus <ms2asdk@gmail.com>
2017-04-23 04:26:42 -0700

    Interpreter example

---
+++ interpreter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Interpreter
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var sandwhich = new Sandwhich(
+                new WheatBread(),
+                new CondimentList(
+                    new List<Condiment> {new MayoCondiment(), new MustardCondiment()}),
+                new IngredientList(
+                    new List<Ingredient> {new LettuceIngredient(), new ChickenIngredient()}),
+                new CondimentList(new List<Condiment> {new KetchupCondiment()}),
+                new WheatBread());
+
+            sandwhich.Interpret(new Context());
+
+            Console.ReadKey();
+        }
+    }
+}
