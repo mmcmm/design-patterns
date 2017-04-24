b157be880e2e20e20b77ab114c76059c83219fde
marcus <ms2asdk@gmail.com>
2017-04-24 01:30:28 -0700

    add a complicate object

---
+++ prototype/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Prototype
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var complicatedObject = // can't do new
+
+
+                Console.ReadKey();
+        }
+    }
+}
