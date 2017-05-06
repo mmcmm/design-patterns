956cfa92fbb762007827e98db2c3e8d65bbe27a3
marcus <ms2asdk@gmail.com>
2017-05-06 16:27:35 +0300

    event stock ticker

---
+++ observer/EventAndDelegate/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Observer.EventAndDelegate;
+
+namespace Observer.EventAndDelegate
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+
+		}
+	}
+}
