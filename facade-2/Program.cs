8942907ffbcceebbd5d33eec6a4723faba708def
marcus <ms2asdk@gmail.com>
2017-05-08 11:46:18 +0300

    facade example

---
+++ facade-2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace facade_2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
--- facade/Program.cs
+++ facade/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using TemperatureFacade;
 
 namespace WeatherServices
 {
     internal class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
+            const string zipCode = "83714";
+
+            var temperatureFacade = new TemperatureLookupFacade();
+            LocalTemperature localTemp = temperatureFacade.GetTemperature(zipCode);
+
+            Console.WriteLine("The current temperature is {0}F/{1}C. in {2}, {3}",
+                localTemp.Farenheit.ToString("F1"),
+                localTemp.Celcius.ToString("F1"),
+                localTemp.City,
+                localTemp.State);
         }
     }
 }
