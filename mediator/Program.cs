6d7d9191edfa34fb09b022c496a4022ee932f34d
marcus <ms2asdk@gmail.com>
2017-05-14 14:21:28 +0300

    mediator example

--- mediator/Program.cs
+++ mediator/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace mediator
+namespace Mediator
 {
-    internal class Program
+    public class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
+            var flight1 = new Airbus321("AC159");
+            var flight2 = new Boeing737200("WS203");
+            flight2.Acknowledges(flight1);
+            flight1.Acknowledges(flight2);
+
+            var flight3 = new Embraer190("AC602");
+            flight3.Acknowledges(flight1);
+            flight1.Acknowledges(flight3);
+            flight3.Acknowledges(flight2);
+            flight2.Acknowledges(flight3);
         }
     }
 }
