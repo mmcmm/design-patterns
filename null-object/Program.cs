96932fdf0a8b92bfb26ad19007927ec2ee151748
marcus <ms2asdk@gmail.com>
2017-05-14 14:53:45 +0300

    auto repository

---
+++ null-object/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace null_object
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
