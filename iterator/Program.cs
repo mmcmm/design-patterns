7f6062d619574e665f577fbfdcf35d4130ad2eef
marcus <ms2asdk@gmail.com>
2017-05-15 11:37:22 +0300

    tree iterators

---
+++ iterator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Iterator
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
